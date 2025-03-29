1f3a5ebfe54c2e1af34991e9281ac15418e94b08
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2025-03-29 16:48:30 +0530

    Final Deploy

---
+++ app.py
@@ -0,0 +1,54 @@
+from agents import guide_expert, location_expert, planner_expert
+from tasks import location_task, guide_task, planner_task
+from crewai import Crew, Process
+import streamlit as st
+from pyngrok import ngrok
+
+
+st.title("✈️ Your's Trip Planner Agent")
+
+st.markdown("""
+### 💡 **Plan Your Next Trip with AI!**  
+""")
+
+from_city = st.text_input("🏡 From Country", "Dubai")
+destination_city = st.text_input("✈️ Destination Country", "India")
+date_from = st.date_input("📅 Departure Date")
+date_to = st.date_input("📅 Return Date")
+interests = st.text_area("🎯 Your Interests (Nature, Food, Adventure) 🍕🏞️🎢", "Adventure & Great Food")
+
+public_url = ngrok.connect(8501)
+print(public_url)
+
+if st.button("🚀 Generate Travel Plan"):
+    if not from_city or not destination_city or not date_from or not date_to or not interests:
+        st.error("⚠️ Please fill in all fields before generating your travel plan.")
+    else:
+        st.write("⚙️Preparing Your Plan.....(Just 2 Min 😊)")
+
+        loc_task = location_task(location_expert, from_city, destination_city, date_from, date_to)
+        guid_task = guide_task(guide_expert, destination_city, interests, date_from, date_to)
+        plan_task = planner_task([loc_task, guid_task], planner_expert, destination_city, interests, date_from, date_to)
+
+        crew = Crew(
+            agents=[location_expert, guide_expert, planner_expert],
+            tasks=[loc_task, guid_task, plan_task],
+            process=Process.sequential,
+            full_output=True,
+            verbose=True,
+        )
+
+        result = crew.kickoff()
+
+        st.subheader("✅ Your AI-Powered Travel Plan")
+        st.markdown(result)
+
+
+        travel_plan_text = str(result)  
+
+        st.download_button(
+            label="📥 Download Travel Plan",
+            data=travel_plan_text,  
+            file_name=f"Travel_Plan_{destination_city}.txt",
+            mime="text/plain"
+        )
